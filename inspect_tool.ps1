80daaefdf1525f6a1a8e47c131c32888404b7401
creature-creature <168889478+creature-creature@users.noreply.github.com>
2026-08-03 13:50:48 +0900

    Revert IToolPlugin implementation to fix YMM4 UI layout issue

--- inspect_tool.ps1
+++
@@ -1,7 +0,0 @@
-$asm = [System.Reflection.Assembly]::LoadFrom('C:\YukkuriMovieMaker4-20231229T073048Z-001\YukkuriMovieMaker4\YukkuriMovieMaker.Plugin.dll')
-$asm.GetExportedTypes() | Where-Object { $_.Name -like '*Tool*' } | ForEach-Object {
-    Write-Host $_.FullName
-    foreach ($m in $_.GetMembers()) {
-        Write-Host "  $m"
-    }
-}
