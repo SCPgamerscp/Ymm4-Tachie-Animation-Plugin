528a9ba86c0333c63602c8da278957172994c8d0
creature-creature <168889478+creature-creature@users.noreply.github.com>
2026-08-07 17:56:18 +0900

    Update plugin with latest changes and rebuild ymme package

--- src/Ymm4TachieAnimationPlugin.Plugin/BoneAnimationTachiePlugin.cs
+++ src/Ymm4TachieAnimationPlugin.Plugin/BoneAnimationTachiePlugin.cs
@@ -7,7 +7,7 @@ namespace Ymm4TachieAnimationPlugin.Plugin;
 
 public sealed class BoneAnimationTachiePlugin : ITachiePlugin
 {
-    public string Name => "2Dボーンアニメーション立ち絵";
+    public string Name => "2D\u30DC\u30FC\u30F3\u30A2\u30CB\u30E1\u30FC\u30B7\u30E7\u30F3\u7ACB\u3061\u7D75";
 
     public ITachieCharacterParameter CreateCharacterParameter() => new BoneCharacterParameter();
     public ITachieItemParameter CreateItemParameter() => new BoneItemParameter();
--- src/Ymm4TachieAnimationPlugin.Plugin/Parameters/BoneCharacterParameter.cs
+++ src/Ymm4TachieAnimationPlugin.Plugin/Parameters/BoneCharacterParameter.cs
@@ -7,7 +7,7 @@ namespace Ymm4TachieAnimationPlugin.Plugin.Parameters;
 
 internal sealed class BoneCharacterParameter : TachieCharacterParameterBase
 {
-    [Display(Name = "リグフォルダー", Description = "rig.json、テクスチャ、モーションを格納したフォルダー")]
+    [Display(Name = "\u30EA\u30B0\u30D5\u30A9\u30EB\u30C0\u30FC", Description = "rig.json\u3001\u30C6\u30AF\u30B9\u30C1\u30E3\u3001\u30E2\u30FC\u30B7\u30E7\u30F3\u3092\u683C\u7D0D\u3057\u305F\u30D5\u30A9\u30EB\u30C0\u30FC")]
     [DirectorySelector]
     public string? DirectoryPath
     {
@@ -16,8 +16,8 @@ internal sealed class BoneCharacterParameter : TachieCharacterParameterBase
     }
     private string? directoryPath;
 
-    [Display(Name = "PSDファイル", Description = "PSDファイルを直接指定する場合はこちら。指定した場合はリグフォルダーより優先され、自動でリグに変換されます。")]
-    [FileSelector(FileGroupType.TachieParts, CustomFilterName = "PSDファイル", CustomFilterValue = "*.psd;*.psb")]
+    [Display(Name = "PSD\u30D5\u30A1\u30A4\u30EB", Description = "PSD\u30D5\u30A1\u30A4\u30EB\u3092\u76F4\u63A5\u6307\u5B9A\u3059\u308B\u5834\u5408\u306F\u3053\u3061\u3089\u3002\u6307\u5B9A\u3057\u305F\u5834\u5408\u306F\u30EA\u30B0\u30D5\u30A9\u30EB\u30C0\u30FC\u3088\u308A\u512A\u5148\u3055\u308C\u3001\u81EA\u52D5\u3067\u30EA\u30B0\u306B\u5909\u63DB\u3055\u308C\u307E\u3059\u3002")]
+    [FileSelector(FileGroupType.TachieParts, CustomFilterName = "PSD\u30D5\u30A1\u30A4\u30EB", CustomFilterValue = "*.psd;*.psb")]
     public string? PsdFilePath
     {
         get => psdFilePath;
